2e1fb9f8e1743ebd6fb3fff38d230003d7c5d8e5
ayushdoesmarketing <ayush007.pps@gmail.com>
2025-09-29 13:58:08 +0530

    Styled links as animated buttons

--- vite-project/src/components/Links.jsx
+++ vite-project/src/components/Links.jsx
@@ -1,18 +1,34 @@
-import React from 'react';
+
 
 function Links() {
-    return (
-        <div className="card">
-            <h2> MY LINKS </h2>
-            <ul>
-                <li><a href = "https://www.linkedin.com/in/ayush-negi-089001287/"   target = "_blank" rel="noreferrer">LinkedIn</a></li>        
-                <li><a href = "https://github.com/AyushdoesCoding"   target = "_blank" rel="noreferrer">GitHub</a></li>
-                <li><a href = "https://x.com/AyushhTweets"   target = "_blank" rel="noreferrer">Twitter</a></li>
-                <li><a href = "https://www.instagram.com/ayushhh__negi/"   target = "_blank" rel="noreferrer">Instagram</a></li>
-                <li><a href = " mailto:Ayushnegi2k05@gmail.com"> Email me </a></li>
-            </ul>
-        </div>
-
-    );
+return (
+    <div className="card">
+    <h2>MY LINKS</h2>
+      {/* We add a className to the list itself */}<ul className="links-list">
+        <li>
+          {/* And we add a className to each link to turn it into a button */}
+        <a href="https://www.linkedin.com/in/ayush-negi-089001287/" className="link-as-button" target="_blank" rel="noopener noreferrer">LinkedIn
+        </a>
+        </li>
+        <li>
+        <a href="https://github.com/AyushdoesCoding" className="link-as-button" target="_blank" rel="noopener noreferrer">GitHub
+        </a>
+        </li>
+        <li>
+        <a href="https://x.com/AyushhTweets" className="link-as-button" target="_blank" rel="noopener noreferrer">Twitter
+        </a>
+        </li>
+        <li>
+        <a href="https://www.instagram.com/ayushhh__negi/" className="link-as-button" target="_blank" rel="noopener noreferrer">Instagram
+        </a>
+        </li>
+        <li>
+        <a href="mailto:Ayushnegi2k05@gmail.com" className="link-as-button">Email me</a>
+        </li>
+    </ul>
+</div>
+);
 }
+
 export default Links;
+
